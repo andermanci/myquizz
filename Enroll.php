97f3f60dab1fa33e6ba3bbf510f736113255486a
“UrtziOtamendi” <urtziotamendi@gmail.com>
2016-10-01 17:12:31 +0200

    My quizz PHP and MySQL

--- Enroll.php
+++ Enroll.php
@@ -1,10 +1,19 @@
 
 <?php
-	$link = mysqli_connect("mysql.hostinger.es","u562857369_ander","mancisidor","u562857369_quizz");
-	$sql = "INSERT INTO Erabiltzaile VALUES ('$_POST[izen]','$_POST[abizen]','$_POST[email]','$_POST[pass]','$_POST[tfl]','$_POST[Adarra]','$_POST[gehigarria]','$_POST[argazkia]')";
+	$link = mysqli_connect("mysql.hostinger.es","u883675844_urtzi","Otamendi","u883675844_quizz");
+	if (getimagesize($_FILES['argazkia']['tmp_name'])==FALSE) {
+		echo "Errorea: Aukeratu argazkia.";
+	}
+	else{
+		
+		$nombre_img = $_FILES['argazkia']['name'];
+		$directorio = $_SERVER['DOCUMENT_ROOT'].'/php/image/';
+		move_uploaded_file($_FILES['argazkia']['tmp_name'],$directorio.$nombre_img);
+	}
+	$sql = "INSERT INTO Erabiltzaile VALUES ('$_POST[izen]','$_POST[abizen]','$_POST[email]','$_POST[pass]','$_POST[tfl]','$_POST[Adarra]','$_POST[gehigarria]','$nombre_img')";
 	$ema = mysqli_query($link,$sql);
 	if (!$ema){ 
-		die('Errorea query-a gauzatzerakoan: ' . msqli_error());
+		die('Errorea query-a gauzatzerakoan: ' .mysqli_error($link));
 	}
 	echo "Erregistro bat gehitu da";
 	echo "<p> <a href='ShowUsers.php'> Erregistroak ikusi</a>";
