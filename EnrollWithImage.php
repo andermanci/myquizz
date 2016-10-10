d0625f44a9f73728d4332e5d9b6653abc804770b
“UrtziOtamendi” <urtziotamendi@gmail.com>
2016-10-10 12:59:23 +0200

    Lab 3 derrigorrezkoa

--- EnrollWithImage.php
+++ EnrollWithImage.php
@@ -1,22 +1,29 @@
 
 <?php
 	$link = mysqli_connect("mysql.hostinger.es","u883675844_urtzi","Otamendi","u883675844_quizz");
-	if (getimagesize($_FILES['argazkia']['tmp_name'])==FALSE) {
-		echo "Errorea: Aukeratu argazkia.";
+	if(filter_var($_POST[email], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(([^<>()\[\]\\.,;:\s@]+(\.[^<>()\[\]\\.,;:\s@]+)*)|(.+))[0-9]{3}@ikasle.ehu.[eus|es]/")))== FALSE){
+		echo("$_POST[email] is not a valid email address");
 	}
-	else{
-		
-		$nombre_img = $_FILES['argazkia']['name'];
-		$directorio = $_SERVER['DOCUMENT_ROOT'].'/php/image/';
-		move_uploaded_file($_FILES['argazkia']['tmp_name'],$directorio.$nombre_img);
+	else {
+		if (getimagesize($_FILES['argazkia']['tmp_name'])==FALSE) {
+			echo "Errorea: Aukeratu argazkia.";
+		}
+		else{
+			
+			$nombre_img = $_FILES['argazkia']['name'];
+			$directorio = $_SERVER['DOCUMENT_ROOT'].'/php/image/';
+			move_uploaded_file($_FILES['argazkia']['tmp_name'],$directorio.$nombre_img);
+		}
+		$sql = "INSERT INTO Erabiltzaile VALUES ('$_POST[izen]','$_POST[abizen]','$_POST[email]','$_POST[pass]','$_POST[tfl]','$_POST[Adarra]','$_POST[gehigarria]','$nombre_img')";
+		$ema = mysqli_query($link,$sql);
+		if (!$ema){ 
+			die('Errorea query-a gauzatzerakoan: ' .mysqli_error($link));
+		}
+		echo "Erregistro bat gehitu da";
+		echo "<p> <a href='ShowUsersWithImage.php'> Erregistroak ikusi</a>";
+	
+	
 	}
-	$sql = "INSERT INTO Erabiltzaile VALUES ('$_POST[izen]','$_POST[abizen]','$_POST[email]','$_POST[pass]','$_POST[tfl]','$_POST[Adarra]','$_POST[gehigarria]','$nombre_img')";
-	$ema = mysqli_query($link,$sql);
-	if (!$ema){ 
-		die('Errorea query-a gauzatzerakoan: ' .mysqli_error($link));
-	}
-	echo "Erregistro bat gehitu da";
-	echo "<p> <a href='ShowUsersWithImage.php'> Erregistroak ikusi</a>";
 	mysqli_close($link);
 ?>
 
